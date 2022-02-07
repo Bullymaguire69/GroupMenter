2d0e86b65b67d7ca419edbf1d97af0f9f7d35e02
Bullymaguire69 <97878557+Bullymaguire69@users.noreply.github.com>
2022-02-07 14:11:27 +0530

    Update __main__.py

--- GroupMenter/__main__.py
+++ GroupMenter/__main__.py
@@ -74,7 +74,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hey there! My name is Group Menter.
+Hey there! My name is Bully Maguire Bot.
 I'm here to help you manage your groups!
 
 Hit /help to find out more about how to use me to my full potential.
